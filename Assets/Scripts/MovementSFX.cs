9f09e0bfc2fc026e59729b282e83ba696539fd38
hiitsame <bacamantef@gmail.com>
2024-04-20 19:10:04 -0400

    BG & SFX for dark forest and desert

--- Assets/Scripts/MovementSFX.cs
+++ Assets/Scripts/MovementSFX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MovementSFX : MonoBehaviour
 {
    public AudioSource footstep, jump;
-
+   //Fix later: footstep audio should stop when character jumps. Currently, it still plays footstep SFX when pressing directional keys while in air
    void Update() {
     if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
         footstep.enabled = true;
@@ -16,6 +16,7 @@ public class MovementSFX : MonoBehaviour
     }
 
     if (Input.GetKey(KeyCode.Space)) {
+        footstep.enabled = false;
         jump.enabled = true;
     } else {
         jump.enabled = false;
