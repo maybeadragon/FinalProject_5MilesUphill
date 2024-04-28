c6d633664e8bc1452262c1d960f1242a16681577
hiitsame <bacamantef@gmail.com>
2024-04-28 17:38:12 -0400

    Stalker animator controller

--- Assets/Scripts/StalkerAIStateMachine.cs
+++ Assets/Scripts/StalkerAIStateMachine.cs
@@ -5,6 +5,7 @@ public class StalkerStateMachine : MonoBehaviour
 {
     public StalkerState currentState;
     public NavMeshAgent stalker;
+    public Animator animator;
 
     private void Awake() 
     {
@@ -16,6 +17,20 @@ public class StalkerStateMachine : MonoBehaviour
 
     public void SetState(StalkerState newState) 
     {
+        //Set the parameter true/false in StalkerAnimatorController, depending on newState
+        if (newState is StalkerPatrol) {
+            animator.SetBool("Pursuit", false);
+            animator.SetBool("Patrol", true);
+        }
+        else if (newState is StalkerPursuit) {
+            animator.SetBool("Pursuit", true);
+            animator.SetBool("Patrol", false);
+        }
+        else {
+            animator.SetBool("Pursuit", false);
+            animator.SetBool("Patrol", false);
+        }
+
         currentState?.Exit();
         currentState = newState;
         currentState.Enter();
