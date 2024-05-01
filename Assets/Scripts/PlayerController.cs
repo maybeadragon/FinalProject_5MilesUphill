b92b790447343009da2e0a56d40071cb5e0b1f54
hiitsame <149597709+hiitsame@users.noreply.github.com>
2024-05-01 15:50:42 -0400

    PlayerController is now for itemcollection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-/// <summary>
-/// Player Controller, Item Collection, Basic movement (No animations)
-/// </summary>
-/// 
+/*
+ * Item collection script
+ */
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 7f;
     private bool hasKeyItem = false;
-    public float jump = 10f;
-    private bool onFloor;
 
     private void OnTriggerEnter(Collider item)
     {
@@ -35,16 +30,10 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        //To extend, copy above code and use different tags (Make sure to tag the item object)
+
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        //Make sure floor is tagged "Floor"
-        if (collision.gameObject.CompareTag("Floor"))
-        {
-            onFloor = true;
-        }
-    }
 
     private void OnCollectKeyItem()
     {
@@ -64,31 +53,5 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 forward = Camera.main.transform.forward;
-        Vector3 right = Camera.main.transform.right;
-
-        forward.y = 0f;
-        right.y = 0f;
-        forward.Normalize();
-        right.Normalize();
-
-        Vector3 move = forward * verticalInput + right * horizontalInput;
-
-        transform.Translate(move * speed * Time.deltaTime, Space.World);
-
-        //Jump, by default is spacebar
-        /*
-        To adjust jump strength, change the 'jump' variable or the 'Mass' of the player object in Rigidbody
-        jump = 10, Mass = 2 seems pretty good to me
-        */
-        if (Input.GetButtonDown("Jump") && onFloor)
-        {
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
-            onFloor = false;
-        }
     }
 }
